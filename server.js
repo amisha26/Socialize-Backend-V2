fdc30eac69b2ad32d9307cfe2edf465b8100beb3
amisha26 <amisha8299@gmail.com>
2023-12-28 21:49:08 -0500

    feat(posts) -  upload file done

--- server.js
+++ server.js
@@ -6,18 +6,31 @@ import commentRoutes from "./routes/comments.js";
 import authRoutes from "./routes/auth.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
-
-const app = express();
+import multer from "multer";
 
 
 // middlewares
+const app = express();
 app.use(express.json())
 app.use(cors())
 app.use(cookieParser())
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'upload/')
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + file.originalname)
+    }
+  })
+  const upload = multer({ storage: storage })
+
 
 // Routes
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    const file = req.file
+    res.status(200).json(file.filename)
+})
 app.use("/api/users", userRoutes)
 app.use("/api/posts", postRoutes)
 app.use("/api/comments", commentRoutes)
